de2d1f8dc5635ce97b49d55c6322c3ad641132b7
Sonny Williams <sonnywilliams@hotmail.co.uk>
2025-05-25 09:15:04 +0100

    Fixed merging

--- Assets/Scripts/BoardObject.cs
+++ Assets/Scripts/BoardObject.cs
@@ -37,7 +37,7 @@ public abstract class BoardObject : MonoBehaviour, ISaveable
 
     public virtual void EndDrag(Vector2 touchPosition)
     {
-        var cell = GridManager.GetClosestCell(touchPosition, false);
+        var cell = GridManager.GetClosestCell(touchPosition, true);
 
         if (cell.heldObject != null && cell.heldObject != this)
         {
