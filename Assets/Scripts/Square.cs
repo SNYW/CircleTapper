f7b0e69b83f244984b5d65037e5584f1899de8d6
Sonny Williams <sonnywilliams@hotmail.co.uk>
2025-05-25 22:19:33 +0100

    Update Square.cs

--- Assets/Scripts/Square.cs
+++ Assets/Scripts/Square.cs
@@ -1,8 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Globalization;
-using DG.Tweening;
 using Persistence;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Square : BoardObject
@@ -16,11 +16,13 @@ public class Square : BoardObject
     private static readonly int SegmentCount = Shader.PropertyToID("_SegmentCount");
 
     private int _remainingCooldown;
-    private float LerpValue => (float)_remainingCooldown / clickSpeed;
+    private float currentRemovedSegments;
+    private float targetRemovedSegments;
+    private float lerpSpeed = 10f;
 
     public List<GridManager.Direction> tapTargets;
 
-    public FMODUnity.EventReference SquareCompleteSFX; //audio 
+    public FMODUnity.EventReference SquareCompleteSFX;
 
     public override void Init()
     {
@@ -32,17 +34,17 @@ public class Square : BoardObject
         _remainingCooldown = remainingCooldown;
         _propertyBlock = new MaterialPropertyBlock();
         spriteRenderer.GetPropertyBlock(_propertyBlock);
-        
-        _propertyBlock.SetFloat(SegmentCount, clickSpeed);
-        _propertyBlock.SetFloat(RemovedSegments, LerpValue);
-        
-        spriteRenderer.SetPropertyBlock(_propertyBlock);
-        
-        if(parentCell == null)
-            GridManager.GetClosestCell(transform.position).SetChildObject(this);
-        StartCoroutine(ClickNeighbours());
 
-        LerpRemovedSegments(LerpValue);
+        _propertyBlock.SetFloat(SegmentCount, clickSpeed);
+        currentRemovedSegments = (float)_remainingCooldown / clickSpeed;
+        targetRemovedSegments = currentRemovedSegments;
+        _propertyBlock.SetFloat(RemovedSegments, currentRemovedSegments);
+        spriteRenderer.SetPropertyBlock(_propertyBlock);
+
+        if (parentCell == null)
+            GridManager.GetClosestCell(transform.position).SetChildObject(this);
+
+        StartCoroutine(ClickNeighbours());
     }
 
     public override void BeginDrag(Vector2 startPos)
@@ -62,70 +64,53 @@ public class Square : BoardObject
     {
         while (gameObject.activeSelf)
         {
-            yield return new WaitForSeconds(1);
+            yield return new WaitForSeconds(1f);
 
             _remainingCooldown--;
-            LerpRemovedSegments((float)_remainingCooldown/clickSpeed);
+            targetRemovedSegments = Mathf.Clamp01((float)_remainingCooldown / clickSpeed);
             SaveObjectState();
 
             if (_remainingCooldown > 0) continue;
-            if(parentCell == null) continue;
-            
+            if (parentCell == null) continue;
+
             foreach (var direction in tapTargets)
             {
                 if (!parentCell.Neighbors.TryGetValue(direction, out var neighbor)) continue;
                 if (neighbor.heldObject is Circle circle)
                 {
                     circle.OnTap();
-                    FMODUnity.RuntimeManager.PlayOneShotAttached(SquareCompleteSFX, gameObject); //audio
+                    FMODUnity.RuntimeManager.PlayOneShotAttached(SquareCompleteSFX, gameObject);
                 }
                 else
                 {
-                    FMODUnity.RuntimeManager.PlayOneShotAttached(SquareCompleteSFX, gameObject); //audio
+                    FMODUnity.RuntimeManager.PlayOneShotAttached(SquareCompleteSFX, gameObject);
                 }
-                
             }
 
             foreach (var clickParticle in clickParticles)
             {
                 clickParticle.Play();
             }
-                
-            transform.DOScale(1.2f, 0.1f)
-                .SetEase(Ease.OutQuad)
-                .OnComplete(() => transform.DOScale(1f, 0.1f)
-                    .SetEase(Ease.InQuad)
-                    .SetTarget(gameObject))
-                .SetTarget(gameObject);
-                
-            _remainingCooldown = clickSpeed;
-            LerpRemovedSegments(1);
-        }
-    }
-    
-    private void LerpRemovedSegments(float newValue)
-    {
-        if (spriteRenderer == null) return;
-        
-        spriteRenderer.GetPropertyBlock(_propertyBlock);
-        
-        if (_propertyBlock.HasProperty(RemovedSegments))
-        {
-            float currentRemovedSegments = _propertyBlock.GetFloat(RemovedSegments);
 
-            DOTween.To(() => currentRemovedSegments, value =>
-                {
-                    _propertyBlock.SetFloat(RemovedSegments, value);
-                    spriteRenderer.SetPropertyBlock(_propertyBlock);
-                }, newValue, 0.2f)
-                .SetTarget(gameObject);
-        }
-        else
-        {
-            Debug.LogWarning("MaterialPropertyBlock does not have a '_RemovedSegments' property.");
+            transform.localScale = Vector3.one * 1.2f;
+            yield return new WaitForSeconds(0.1f);
+            transform.localScale = Vector3.one;
+
+            _remainingCooldown = clickSpeed;
+            targetRemovedSegments = 1f;
         }
     }
-    
+
+    private void Update()
+    {
+        if (Mathf.Approximately(currentRemovedSegments, targetRemovedSegments)) return;
+
+        currentRemovedSegments = Mathf.MoveTowards(currentRemovedSegments, targetRemovedSegments, Time.deltaTime * lerpSpeed);
+
+        _propertyBlock.SetFloat(RemovedSegments, currentRemovedSegments);
+        spriteRenderer.SetPropertyBlock(_propertyBlock);
+    }
+
     public override BoardObjectSaveData ToSaveData()
     {
         return new BoardObjectSaveData
@@ -141,20 +126,24 @@ public class Square : BoardObject
 
     public override void FromSaveData(BoardObjectSaveData saveData)
     {
-        DOTween.KillAll(gameObject);
         StopAllCoroutines();
-        
+
         _remainingCooldown = saveData.value;
         GridManager.GetGridCell(new Vector2Int(saveData.xPosition, saveData.yPosition)).SetChildObject(this);
         Init(saveData.value);
     }
-    
+
     protected override void SaveObjectState()
     {
-        if(parentCell != null)
+        if (parentCell != null)
             SaveManager.Instance.AddObject(parentCell.gridPosition, ToSaveData());
     }
-    
+
+    private void OnDisable()
+    {
+        StopAllCoroutines();
+    }
+
     public override string GetValue()
     {
         return _remainingCooldown.ToString();
